c092187804f2bc13c99e38360c2e68d38bd5b321
Akhmatamin <ahmatamin.raimzhanov@alatoo.edu.kg>
2026-04-11 23:53:38 +0600

    Nothing important!

--- leet.py
+++ leet.py
@@ -1,3 +1,30 @@
-ls = [3,1,4,1,5,9,2,6]
-ls.remove(max(ls))
-print(max(ls))
+from typing import Callable
+import time
+from functools import wraps
+
+
+def limit_call_deco1(limit : int):
+    def wrapper(func : Callable):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            """Non-necessary docu"""
+            nonlocal limit
+            if limit == 0:
+                print('Cannot call function!')
+                return
+            start = time.time()
+            res = func(*args, **kwargs)
+            print(f'{func.__name__} took {time.time() - start} seconds')
+            limit -= 1
+            return res
+        return inner
+    return wrapper
+
+@limit_call_deco1(2)
+def my_func(sleep_time: int):
+    """Some documentation very important"""
+    time.sleep(sleep_time)
+    return "Hello My Decorator!"
+print(my_func(1))
+print(my_func.__doc__)
+print(my_func.__name__)
